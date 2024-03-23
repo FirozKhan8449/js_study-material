914757890750c55e8542102023b16aa3c78c1d96
FirozKhan8449 <163640515+FirozKhan8449@users.noreply.github.com>
2024-03-23 04:26:06 +0000

    Datatypes

---
+++ 01-js_study/DataTypes_EcmaScript.js
@@ -0,0 +1,31 @@
+"use strict"   //use js as newer version
+//Node : Two Document that we should know 1.MDN by Mozilla Organization and 2.TC39.js By Ecmascript standard
+// let false="kdkfdkf"
+// console.log(false);//we cannot use the reserved words as variable declaration
+
+
+//DataTypes in js 
+//1.Primitive Data Type
+
+/*
+a.string  =>""
+b.number  =>1,2.4
+c.boolean  =>true/false
+d.null      =>it is a standalone value
+e.undefinded => vaue not defined
+f.bigInt     =>when the ranges crosses 2 to the power of 53 we use bigint
+g.symbol     =>uniquelly defined
+
+
+//Non Primitive Data Types
+// 1.Array
+// 2.Object
+*/
+
+console.log(typeof String)   //Function
+console.log(typeof undefined)//undefined
+console.log(typeof null)     //object
+console.log(typeof Symbol)   //function
+console.log(typeof Number)   //function
+console.log(typeof BigInt)   //function
+console.log(typeof Boolean)  //function
---
+++ 01-js_study/variables.js
@@ -0,0 +1,7 @@
+const account_Id=12345
+let accountEmail="fzkhan@gmail.com"
+var accountCity="Tappal"
+accountState="UP"
+
+// account_Id=3  ///this is not allowed
+console.log(account_Id)
